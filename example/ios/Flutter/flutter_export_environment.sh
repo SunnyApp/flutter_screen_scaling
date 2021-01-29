aeb4c386817debbbeb809a04715d25a9a5ebe033
ericmartineau <smartytime@gmail.com>
2021-01-29 11:25:14 -0700

    Generated file

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/ericm/fvm/versions/1.20.2"
-export "FLUTTER_APPLICATION_PATH=/Users/ericm/sunny/plugin/flutter_screen_scaling/example"
+export "FLUTTER_ROOT=/Users/ericm/fvm/versions/master"
+export "FLUTTER_APPLICATION_PATH=/Users/ericm/sunny/local_plugins/flutter_screen_scaling/example"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
-export "FLUTTER_FRAMEWORK_DIR=/Users/ericm/fvm/versions/1.20.2/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
 export "DART_OBFUSCATION=false"
